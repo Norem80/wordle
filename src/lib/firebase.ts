773d9589e1138132534ac34817df7b399bb813e8
Nikodem Deja <nikodem@nikodemdeja.pl>
2023-02-21 14:49:27 +0100

    Add needed firebase functions

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,15 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  getCount,
+  getDoc,
+  doc,
+  query,
+  where
+} from 'firebase/firestore/lite';
+import { describe } from 'vitest';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -11,10 +21,56 @@ initializeApp(firebaseConfig);
 
 const db = getFirestore();
 
+export interface WordModel {
+  word: string;
+}
+
 export async function getMessage() {
   const wordsCollection = collection(db, 'test');
   const wordsSnapshot = await getDocs(wordsCollection);
   const wordsList = wordsSnapshot.docs.map((doc) => doc.data());
 
   return wordsList;
+}
+
+export async function getRandomWord() {
+  const wordsCollection = collection(db, 'words');
+  const countSnapshot = await getCount(wordsCollection);
+  const wordsCount = countSnapshot.data().count;
+
+  const randomWordId = Math.floor(Math.random() * (wordsCount - 1));
+
+  const randomWordDoc = await getDoc(doc(db, 'words', `${randomWordId}`));
+  const randomWordData = randomWordDoc.data() as WordModel | undefined;
+
+  if (randomWordData) return randomWordData.word;
+  return '';
+}
+
+export async function isValidWord(word: string) {
+  const wordsCollection = collection(db, 'words');
+
+  const wordQuery = query(wordsCollection, where('word', '==', word.toLowerCase()));
+  const wordDoc = await getDocs(wordQuery);
+  return wordDoc.size > 0;
+}
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+
+  const randomWord = await getRandomWord();
+
+  describe('Firebase', () => {
+    it('randomWord is not empty', () => {
+      expect(randomWord).toBeTruthy();
+    });
+
+    it('randomWord is valid word', async () => {
+      expect(await isValidWord(randomWord)).toBe(true);
+    });
+
+    it('word not from the list is not valid', async () => {
+      expect(await isValidWord('notValid')).toBe(false);
+    });
+  });
 }
